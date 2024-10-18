467897cb74e9856e2369c55ccdca1e6e690fc7d5
alexander Berhe <134514212+alecomedhanie@users.noreply.github.com>
2024-10-18 12:03:53 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,8 +1,12 @@
- provider "aws" {
-  region = "us-east-2"
+resource "aws_flow_log" "vpc_flow_log" {
+  log_destination_type = "cloud-watch-logs"
+  vpc_id               = aws_vpc.my_vpc.id
+  traffic_type         = "ALL"
+  
+  log_group_name       = aws_cloudwatch_log_group.vpc_flow_logs.name
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/24"
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name = "vpc-flow-logs"
+  retention_in_days = 7
 }
-
