56d1a31814c489451026a4f8293f4203fc85150a
alexander Berhe <134514212+alecomedhanie@users.noreply.github.com>
2024-10-19 07:42:28 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,8 +1,6 @@
 # Create VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block           = "10.0.0.0/24"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 
   tags = {
     Name        = "my-vpc"
